d62fc6db5947f2d45cff652bcd147cf002376511
Chandulajeewantha2003 <chandulajeewantha2003@gmail.com>
2025-08-21 11:24:20 +0530

    Main home scrool view

--- app/src/main/java/com/example/fitpro/MainHome.kt
+++ app/src/main/java/com/example/fitpro/MainHome.kt
@@ -1,33 +1,20 @@
 package com.example.fitpro
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity7 : AppCompatActivity() {
+class MainHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main7)
-
-        // Apply edge-to-edge padding
+        setContentView(R.layout.activity_main_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Find the Get Started button
-        val getStartedButton: Button = findViewById(R.id.button5)
-
-        // Set click listener to open MainHome activity
-        getStartedButton.setOnClickListener {
-            val intent = Intent(this, MainHome::class.java)
-            startActivity(intent)
         }
     }
 }
